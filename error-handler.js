d5688cd8f4002241ed4988be0667e7d8d6cb5ee3
Livingston-immanuel <115205768+Livingston-immanuel@users.noreply.github.com>
2023-01-03 10:31:55 +0530

    Update error-handler.js

--- error-handler.js
+++ error-handler.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const app = express();
-const { ErrorWrapper, ErrorHandler } = require('Error-Handler');
+const { ErrorWrapper, ErrorHandler } = require('@kaavian/error-handler');
 const bodyParser = require ('body-parser');
 const { convert } = require ('./convert');
 
 app.use(bodyParser.json());
 
 
-app.get('/convert', ErrorWrapper(convert));
-
-// app.get('/convert', convert )
+// sync function
+// if error occured in syncronous function the ErrorHandler middleware(in the bottom) will handle the error. No need for try catch..
 
 app.get('/uppercase', (req, res ) => {
     const { data } = req.query;
-//   console.log(text,253);
     const upper = data.toUpperCase();
     res.json(upper);
 
 })
 
+// Async Function 
+// for async function we should wrap the entire function in ErrorWrapper function like given below
 
-
-app.post('/api/getNumber',async(req, res, err, next) => {
+app.post('/api/getNumber',ErrorWrapper(async(req, res) => {
     const { userName , password } = req.body;
-    console.log( userName ,password , 569);
     if(userName === 'livi' && password === 'kaavian'){
        return res.json({ msg: "user verified"})
     }
     return res.json({msg : 'user not found '})
-})
+}));
+
+// this is how it looks when we split the function and using ErrorWrapper.
+
+app.get('/convert', ErrorWrapper(convert));
 
 
+//IMPORTANT NOTE
+//error handler middleware should located below the final route 
 
 app.use(ErrorHandler);
 
